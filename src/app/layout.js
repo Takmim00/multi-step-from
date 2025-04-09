934fea8378d8ada3d64e6353635b947aef5178d6
Takmim00 <takmimm@gmail.com>
2025-04-09 23:22:08 +0600

    darkmode setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/components/provider/transtackQuery";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +25,10 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 
         {children}
+        </ThemeProvider>
         </QueryProvider>
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/themeToggle/themeToggle";
+
+
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 p-4">
       <div className="w-full max-w-2xl">
-        <h1 className="text-3xl font-bold text-center mb-8">
-          Multi-Step Registration Form
-        </h1>
-        <Button variant="outline">from</Button>
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-3xl font-bold text-gray-900 dark:text-white">Multi-Step Registration Form</h1>
+          <ThemeToggle />
+        </div>
+
       </div>
     </main>
-  );
+  )
 }
